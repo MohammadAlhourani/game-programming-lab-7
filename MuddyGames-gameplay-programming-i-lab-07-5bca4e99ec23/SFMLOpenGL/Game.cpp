db181f257b3d49833a25c8ab05352958483df145
MohammadAlhourani <34336016+MohammadAlhourani@users.noreply.github.com>
2018-12-11 10:27:02 +0000

    added matrix rotation

--- MuddyGames-gameplay-programming-i-lab-07-5bca4e99ec23/SFMLOpenGL/Game.cpp
+++ MuddyGames-gameplay-programming-i-lab-07-5bca4e99ec23/SFMLOpenGL/Game.cpp
@@ -82,11 +82,11 @@ void Game::initialize()
 			glVertex3f(faces[4][x].x, faces[4][x].y, faces[4][x].z);
 		}
 
-		for (int x = 0; x < 4; x++)
+		/*for (int x = 0; x < 4; x++)
 		{
 			glColor3f(0.0f, 0.0f, 1.0f);
 			glVertex3f(faces[5][x].x, faces[5][x].y, faces[5][x].z);
-		}
+		}*/
 	}
 	glEnd();
 	glEndList();
@@ -116,43 +116,61 @@ void Game::update()
 			for (int x = 0; x < 4; x++)
 			{
 				glColor3f(0.0f, 1.0f, 0.0f);
+				faces[0][x] = MyMatrix3::rotationZ(rotationAngle) * MyVector3(faces[0][x].x, faces[0][x].y, faces[0][x].z);
+				//faces[0][x] = MyMatrix3::translation(MyVector3(-1,0,0)) * MyVector3(faces[0][x].x, faces[0][x].y, faces[0][x].z);
+				//faces[0][x] = MyMatrix3::scale(2) * MyVector3(faces[0][x].x, faces[0][x].y, faces[0][x].z);
 				glVertex3f(faces[0][x].x, faces[0][x].y, faces[0][x].z);
 			}
 
 			for (int x = 0; x < 4; x++)
 			{
 				glColor3f(1.0f, 0.0f, 0.0f);
+				faces[1][x] = MyMatrix3::rotationZ(rotationAngle) * MyVector3(faces[1][x].x, faces[1][x].y, faces[1][x].z);
+				//faces[1][x] = MyMatrix3::translation(MyVector3(-1, 0, 0)) * MyVector3(faces[1][x].x, faces[1][x].y, faces[1][x].z);
+				//faces[1][x] = MyMatrix3::scale(2) * MyVector3(faces[1][x].x, faces[1][x].y, faces[1][x].z);
 				glVertex3f(faces[1][x].x, faces[1][x].y, faces[1][x].z);
 			}
 
 			for (int x = 0; x < 4; x++)
 			{
 				glColor3f(1.0f, 1.0f, 0.0f);
+				faces[2][x] = MyMatrix3::rotationZ(rotationAngle) * MyVector3(faces[2][x].x, faces[2][x].y, faces[2][x].z);
+				//faces[2][x] = MyMatrix3::translation(MyVector3(-1, 0, 0)) * MyVector3(faces[2][x].x, faces[2][x].y, faces[2][x].z);
+				//faces[2][x] = MyMatrix3::scale(2) * MyVector3(faces[2][x].x, faces[2][x].y, faces[2][x].z);
 				glVertex3f(faces[2][x].x, faces[2][x].y, faces[2][x].z);
 			}
 
 			for (int x = 0; x < 4; x++)
 			{
 				glColor3f(1.0f, 0.0f, 1.0f);
+				faces[3][x] = MyMatrix3::rotationZ(rotationAngle) * MyVector3(faces[3][x].x, faces[3][x].y, faces[3][x].z);
+				//faces[3][x] = MyMatrix3::translation(MyVector3(-1, 0, 0)) * MyVector3(faces[3][x].x, faces[3][x].y, faces[3][x].z);
+				//faces[3][x] = MyMatrix3::scale(2) * MyVector3(faces[3][x].x, faces[3][x].y, faces[3][x].z);
 				glVertex3f(faces[3][x].x, faces[3][x].y, faces[3][x].z);
 			}
 
 			for (int x = 0; x < 4; x++)
 			{
 				glColor3f(0.0f, 1.0f, 1.0f);
+				faces[4][x] = MyMatrix3::rotationZ(rotationAngle) * MyVector3(faces[4][x].x, faces[4][x].y, faces[4][x].z);
+				//faces[4][x] = MyMatrix3::translation(MyVector3(-1, 0, 0)) * MyVector3(faces[4][x].x, faces[4][x].y, faces[4][x].z);
+				//faces[4][x] = MyMatrix3::scale(2) * MyVector3(faces[4][x].x, faces[4][x].y, faces[4][x].z);
 				glVertex3f(faces[4][x].x, faces[4][x].y, faces[4][x].z);
 			}
 
 			for (int x = 0; x < 4; x++)
 			{
 				glColor3f(0.0f, 0.0f, 1.0f);
+				faces[5][x] = MyMatrix3::rotationZ(rotationAngle) * MyVector3(faces[5][x].x, faces[5][x].y, faces[5][x].z);
+			//	//faces[5][x] = MyMatrix3::translation(MyVector3(-1,0,0)) * MyVector3(faces[5][x].x, faces[5][x].y, faces[5][x].z);
+			//  //faces[5][x] = MyMatrix3::scale(2) * MyVector3(faces[5][x].x, faces[5][x].y, faces[5][x].z);
 				glVertex3f(faces[5][x].x, faces[5][x].y, faces[5][x].z);
 			}
 		}
 		glEnd();
 		glEndList();
 
-		rotationAngle += 0.005f;
+		rotationAngle = 0.0005f;
 
 		if (rotationAngle > 360.0f)
 		{
@@ -171,7 +189,7 @@ void Game::draw()
 
 	//cout << "Drawing Cube " << endl;
 	glLoadIdentity();
-	glRotatef(rotationAngle, 0, 0, 1); // Rotates the camera on Y Axis
+	//glRotatef(rotationAngle, 0, 0, 1); // Rotates the camera on Y Axis
 
 	glCallList(1);
 
